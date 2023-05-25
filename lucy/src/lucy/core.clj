436695f7f06c4fae72e0ecf957ab6466274222fc
kkprop <>
2023-05-25 17:12:24 +0800

    try schema

--- lucy/src/lucy/core.clj
+++ lucy/src/lucy/core.clj
@@ -15,13 +15,6 @@
                                     )}}
              {}])
 
-;;(def cfg {:store {:backend :file :path "/tmp/example"}})
-
-;;(def conn (d/connect cfg))
-
-
-
-
 (comment
 
   (http/run-server #(ruuter/route routes %) {:port 8082})
@@ -34,28 +27,103 @@
           :creds-profile "default"
           :endpoint (:datomic-endpoint (utils/agora-conf))})
 
-
 (def client (d/client cfg))
 
-(d/create-database client {:db-name "chat"})
-(def conn (d/connect client {:db-name "chat"}))
+(def dbs (atom {}))
 
-(def q #(d/q % (d/db conn)))
+(defn load-db [db-name]
+  (d/create-database client {:db-name db-name})
+  (d/connect client {:db-name db-name})
+  )
+(defn db [path]
+  (get @dbs path
+       (let [cur (load-db path)]
+         (swap! dbs merge {path cur})
+         cur)))
 
-(def transact (partial d/transact conn))
+(db "chat")
+(db "demo")
 
-(def pull #(d/pull (d/db conn) %))
 
-(def as-of #(d/as-of (d/db conn) %))
-(def since #(d/since (d/db conn) %))
+(defn to-add-expr [xs]
+  (->> xs
+       (map (fn [m]
+              [:db/add (:db/id m)
+               (:key m) (:v m)
+               ])
+            )
+       ))
+(comment
+
+  (def chat-schema
+    [{:db/ident :name
+      :db/valueType :db.type/string
+      :db/cardinality :db.cardinality/one}
+     {:db/ident :chat_id
+      :db/valueType :db.type/string
+      :db/cardinality :db.cardinality/one}])
+
+  (d/transact (db "demo") {:tx-data chat-schema})
+
+  (d/transact (db "demo") {:tx-data [{:name "聊天1"
+                                      :chat_id "111"}
+                                     {:name "聊天2"
+                                      :chat_id "222"}
+                                     {:name "聊天3"
+                                      :chat_id "333"
+                                      :doc "doc3"}]})
+
+  (d/q '[:find ?c ?i
+         :where [?e :name ?c]
+         [?e :chat_id ?i]]
+       (d/db (db "demo")))
+
+;;
+  )
+
+(defn filter-schema-field [schema xs]
+  (let [keys
+        (map :db/ident schema)
+        ])
+  (->> xs
+       (if :key )
+       )
+  )
+
+(defn chat-group []
+  (->> (utils/load-json "chat_group.json")
+       (take 2)
+       ;;(map :doc)
+       (map utils/flatten-entities)
+       ;;(map to-add-expr)
+       ;;(map #(d/transact (db "chat") {:tx-data %}))
+       ;;
+       )
+  ;;
+  )
+
+
+;;to early to expose
+;;(def q #(d/q % (d/db conn)))
+;;(def transact (partial d/transact conn))
+;;(def pull #(d/pull (d/db conn) %))
+;;(def as-of #(d/as-of (d/db conn) %))
+;;(def since #(d/since (d/db conn) %))
+
+
+
+
 
 (comment
   (d/transact)
   (d/db conn)
 
+  (d/q {:query '[:find :block/uid]
+        :args [(d/db conn)]})
+
   (transact {:tx-data [{:db/ident :block/uid
-                          :db/valueType :db.type/uuid
-                          :db/cardinality :db.cardinality/one}]})
+                        :db/valueType :db.type/uuid
+                        :db/cardinality :db.cardinality/one}]})
 
   (def name-schema
     [{:db/ident :block/uid
@@ -66,4 +134,5 @@
   (d/create-database client {:db-name "demo"})
   (def conn (d/connect client {:db-name "demo"}))
 ;; using a connection named conn:
-  (d/transact conn {:tx-data name-schema}))
+  (d/transact conn {:tx-data name-schema})
+  )
