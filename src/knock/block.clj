74b6c1052c1431d9052336582087d0fa6fbf90bd
kkprop <>
2024-03-06 13:04:32 +0800

    still need work

--- src/knock/block.clj
+++ src/knock/block.clj
@@ -20,13 +20,42 @@
 ;; a word for other language
 (defn ->units [x]
   (let [s (str-or-file x)]
-    (count (slurp-lines x))
+    (->>
+     (slurp-lines x)
+     (mapcat seq)
+     )
     ;;
     ))
 
+(defn nx [n f xs]
+  ((apply comp (repeat n f))
+   xs
+   ))
+
+(defn ->phrase
+  ([i n xs]
+   (let  [m (map-on-val count (group-by identity
+                                        (partition-all n (nx i rest xs))))]
+
+     m))
+  ([n xs]
+   ;;first
+   (let [m (->> (range n)
+                (map #(->phrase % n xs))
+                (apply merge))
+         mm (map-on-key #(apply str %) m)
+         ]
+     ;(into (sorted-map-by (fn [a b] (compare (get m b) (get m a)))) m)
+     mm
+     ))
+
+  ([xs]
+   (->phrase 1 xs)))
+
 (comment
-  (->units
-    (->abs-path "~/xj")
-    )
+  (pp
+    (->phrase 5
+              (->units
+                (->abs-path "~/shurangama"))))
   ;;
   )
