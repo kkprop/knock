3d3e1fc09dab188f892737ca486d70fb54c02dd9
kkprop <>
2024-05-07 16:32:21 +0800

    fix

--- src/knock/chat.clj
+++ src/knock/chat.clj
@@ -15,10 +15,10 @@
 (defn ->model-cmd [path]
   (apply join-cmd (str model-dir "/bin/main -m")
          path
-         "-c 4096 --temp 0.7 --repeat_penalty 1.9 "
+         "-c 4096 --temp 0.7 --repeat_penalty 1.3 "
          ;;suppress log
          ;;"2>/dev/null"
-         "-ins -r '###'"
+         "-i -r '###'"
          []
          )
   )
@@ -39,26 +39,58 @@
   (let [_ (touch input-file)
         old *in*]
     (with-open [rdr (clojure.java.io/reader input-file)]
-      (binding [*in* rdr]
+      (binding []
         (let [xs (->> (models)
                       (remove pid-running?))
               m (choose (models) 10)]
-          (println m)
-          (proc/shell {:in rdr} (->model-cmd m))
-          ;(thread! (async-fn (fn [x] (println x)) (->ch *in*)))
+          ;(println m)
+          (thread!
+           (proc/shell (->model-cmd m)))
+          ;(thread! (async-fn (fn [x] (println "got " x "len:" (count x))) (->ch *in*)))
+          ;(thread! (println "try read line") (while true (when-let [x (read-line)] (println "got" x " len:" (count x))) (Thread/sleep 100)))
+          (thread!
+            (println "try")
+            (Thread/sleep 5000)
+            (proc/shell "expect -c 'send -- \\x03'")
+            (proc/shell "expect -c 'send -- \\x03'")
+            (println "stop here"))
+          (thread! (Thread/sleep 6000)
+                   (proc/shell "expect -c 'send -- \"什么是时间\r\n\"'")
+                   (proc/shell "expect -c 'send -- \"什么是空间\r\n\"'")
+                   )
           @(promise))))))
 
 (defn capture-stdin []
   (let [_ (touch input-file)
         old *in*]
     (with-open [r (clojure.java.io/reader input-file)]
-      (binding [*in* r]
+      (binding []
         ;;hijack *in*
         ;(thread! (async-fn (fn [x] (println "got" x) (println "count:" (count x))) (->ch *in*)))
         ;(thread! (async-fn (fn [x] (println "old input got" x)) (->ch old)))
         ;;ok to keep reading now 
-        (thread! (println "try read line") (while true (when-let [x (read-line)] (println "got" x)) (Thread/sleep 100)))
+        ;(thread! (println "try read line") (while true (when-let [x (read-line)] (println "got" x " len:" (count x))) ;(println 'sleeping) (Thread/sleep 100)))
+        ;(thread! (proc/shell "gum" "confirm"))
+        (thread! (choose [:a :b]))
+        (thread! (Thread/sleep 1000) (proc/shell "expect -c 'send -- \"\\x03\"'") (println "send c-c"))
+        (thread! (Thread/sleep 200) (proc/shell "expect -c 'send -- \033\\[B'"))
+        ;(thread! (Thread/sleep 200) (proc/shell "expect -c 'send -- 什么是时间ha? \n'"))
+        ;(thread! (Thread/sleep 200) (proc/shell "expect -c 'send --  \"\n什么是时间? \n\"'"))
         @(promise)))))
+
+(defn ww []
+  ;;^C
+  (spit input-file (str (char 3)) :append true)
+  ;;^L
+  (spit input-file (str (char 12)) :append true)
+  ;;^M return 
+  (spit input-file (str (char 13)) :append true)
+  (spit input-file (str (char 0x50)) :append true)
+  (spit input-file (str "^[[B") :append true)
+  (proc/shell "expect -c 'send -- \n'" )
+  (spit input-file (str "what is time? \n" \return \/) :append true)
+  ;;
+  )
 
 (comment
   (pid-file "gguf")
