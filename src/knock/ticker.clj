c186bc2f3d8b9489f071f1a5a8e9c45f1f07a365
kkprop <>
2025-01-21 19:28:25 +0800

    line

--- src/knock/ticker.clj
+++ src/knock/ticker.clj
@@ -308,6 +308,7 @@
                  (reverse (sort-by :speed (-> (map-on-val compare-frame (group-by :ticker (concat prev cur)))
                                               vals
                                               flatten)))]
+             (println (apply str (repeat 80 "-")))
              (map!! println
               (str/split-lines (pp-hashmap @cache :ticker :speed :volunm :change :market-cap :price)))
              (reset! cache xs)))
